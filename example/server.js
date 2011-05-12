dc7cb3d789327e69af0354d425901f03584525a4
Ivan Shumkov <ivan@shumkov.ru>
2011-05-12 16:05:59 +0400

    fix

--- example/server.js
+++ example/server.js
@@ -4,13 +4,13 @@ try {
     throw 'You need install connect module: "sudo npm install connect"';
 }
 
-var Beseda = require('./../index.js'),
+var Beseda = require('./../server'),
     Monitor = require('./../monitor');
 
 var server = connect.createServer(connect.static(__dirname));
 server.listen(4000);
 
-var beseda = new Beseda({ server : server });
+var beseda = new Beseda({ server : server, monitor : true });
 
 var monitor = new Monitor();
 monitor.listen();
