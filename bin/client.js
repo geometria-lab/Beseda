2bd0052340b14a00614a284e6e7ae91821da5d44
Ivan Shumkov <ivan@shumkov.ru>
2011-05-18 22:27:24 +0400

    refactor client

--- bin/client.js
+++ bin/client.js
@@ -11,75 +11,86 @@ cli.parse({
     ssl  : [ false, 'Enable SSL', 'boolean', false ],
 
     transport : ['t', 'Transport',         'string', 'longPolling'],
-    number    : ['n', 'Number of clients', 'number', 10],
+    number    : ['n', 'Number of clients', 'number', 1],
 
-    interval : ['i', 'Interval of publishing messages in seconds', 'number', 1]
+    verbose   : ['v', 'Display verbose information', 'boolean', true]
 }, ['publish', 'subscribe']);
 
 cli.main(function(args, options) {
-    
     function log(clientId, message) {
-        util.log('Client ' + clientId + ' - ' + message);
+        if (options.verbose) {
+            util.log('Client ' + clientId + ' - ' + message);
+        }
     }
 
-    function publish(client, channel) {
-        client.publish(channel, 'Hello guys!', function(error, message) {
-            if (!error) {
-                log(this.clientId, 'published ' + message.id + ' to ' + channel);
-                setTimeout(publish.bind(null, client, channel), options.interval * 1000);
-            }
-        });
-    }
-
-    //var clients = [];
-
-    var channel = args[0] || '/test';
-    if (cli.command == 'subscribe') {
-        console.log('Subscribe ' + options.number + ' clients to ' + channel);
-    }
-
+    var clients = [];
     for (var i = 0; i < options.number; i++) {
         var client = new Client(options);
         client.on('connection', function() {
             log(this.clientId, 'connected');
         });
         client.on('message', function(channel, data, message) {
-            log(this.clientId, 'received ' + message.id + ' from ' + channel);
+            if (options.verbose) {
+                log(this.clientId, JSON.stringify(message));
+            } else {
+                console.log(data);
+            }
         });
         client.on('error', function(error) {
             log(this.clientId, 'ERROR: ' + error);
         });
+        clients.push(client);
+    }
 
-        switch (cli.command) {
-            case 'subscribe':
+    switch (cli.command) {
+        case 'subscribe':
+            if (!args[0]) {
+                console.log("Channel not present.\n Use: beseda-client subscribe [CHANNEL]");
+                return;
+            }
+
+            var channel = args[0];
+
+            for (var i = 0; i < clients.number; i++) {
                 client.subscribe(channel, function() {
                     log(this.clientId, 'subscribed to ' + channel);
                 });
-                break;
-            case 'publish':
-                publish(client, channel);
-                break;
-        }
-    }
-/*
-    if (cli.command == 'publish') {
-        console.log('Publish from ' + options.number + ' clients to ' + channel);
-        for (var i = 0; i < clients.length; i++) {
-            clients[i].publish(channel, 'Hello guys!', function(error, message) {
-                if (!error) {
-                    log(this.clientId, 'published ' + message.id + ' to ' + channel);
-                    setTimeout()
+            }
+
+            break;
+        case 'publish':
+            var use = 'Use: beseda-client publish CHANNEL MESSAGE [REPEAT] [INTERVAL]';
+
+            if (!args[0]) {
+                console.log("Channel not present.\n" + use);
+                return;
+            }
+
+            if (!args[1]) {
+                console.log("Message not present.\n" + use);
+                return;
+            }
+
+            var channel  = args[0],
+                message  = args[1],
+                repeat   = args[2] || 1,
+                interval = args[3] || 1,
+                i        = 0;
+
+            var intervalLink = setInterval(function() {
+                if (i >= repeat) {
+                    clearInterval(intervalLink);
+                    return;
                 }
-            });
-        }
-
-
-
-        setInterval(function() {
-
-        }, options.interval * 1000);
-
-
+                i++;
+                for (var i = 0; i < clients.number; i++) {
+                    clients[i].publish(channel, message, function(error, m) {
+                        if (!error) {
+                            log(this.clientId, 'published ' + m.id + ' to ' + channel);
+                        }
+                    });
+                }
+            }, interval * 1000);
+            break;
     }
-     */
 });
