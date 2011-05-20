b18453a53295e4ec0daffec8e32b4431f636c5b9
Ivan Shumkov <ivan@shumkov.ru>
2011-05-20 15:37:55 +0400

    remove comments

--- bin/server.js
+++ bin/server.js
@@ -26,34 +26,3 @@ cli.main(function(args, options) {
     var beseda = new Beseda(options);
     beseda.listen();
 });
-// TODO: Run beseda as daemon
-/*
-        var daemon = require("daemon"),
-	fs = require("fs"),
-	sys = require("sys"),
-	http = require("http"),
-	file = "/home/user/dae.pid",//Храним PID
-	f = fs.readFileSync(file),
-	pid,
-	args = process.argv;
-switch (args[2]) {
-        case "stop":
-            process.kill(parseInt(fs.readFileSync(file)));//Считываем PID демона и убиваем процесс
-            sys.puts("Daemon stopped");
-            process.exit(0);
-            break;
-        case "start":
-            pid = daemon.start(f);//Запуск демона
-            daemon.lock(file);//Блокируем файл
-            sys.puts("Daemon started with PID "+pid);
-            break;
-        default:
-            sys.puts("Usage: [start|stop]");
-            process.exit(0);
-}
-//Запускаем сервер
-http.createServer(function(req,res){
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end("hello world");
-
-}).listen(3000,'127.0.0.1');*/
