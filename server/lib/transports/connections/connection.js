496860ff793b431f58ea9264d4c5f83dee027052
Kononenko Sergey <kononencheg@gmail.com>
2011-11-28 19:42:40 +0300

    optimizing

--- server/lib/transports/connections/connection.js
+++ server/lib/transports/connections/connection.js
@@ -1,31 +1,22 @@
 var util = require('util');
 
 var Connection = function(id) {
-	this._id = null;
+	this._id = id;
 	this._transport = null;
-
-	if (id !== undefined) {
-		this._id = id;
-	}
-};
-
-Connection.prototype.setTransport = function(transport) {
-	this._transport = transport;
 };
 
 Connection.prototype.apply = function(request, response, head) {};
 
 Connection.prototype.write = function(data) {};
 
+Connection.prototype.setTransport = function(transport) {
+	this._transport = transport;
+};
+
 Connection.prototype.handleData = function(data) {
 	if (this._transport !== null) {
 		this._transport.emit('message', this._id, JSON.parse(data));
 	}
-};
-
-Connection.prototype.disconnect = function() {
-	this._transport.removeConnection(this._id);
-	this._transport.emit('disconnect', this._id);
 };
 
 module.exports = Connection;
